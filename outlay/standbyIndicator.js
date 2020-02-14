2e1cfce967bd3a1dc6607c790c9d0dec62fbcdd1
tsandrewf <tsandrewf@gmail.com>
2020-02-14 10:00:34 +0300

    Remove static propery from class StandbyIndicator

--- outlay/standbyIndicator.js
+++ outlay/standbyIndicator.js
@@ -1,24 +1,4 @@
-/*export class StandbyIndicator {
-  standbyIndicator = null;
-
-  static show() {
-    if (this.standbyIndicator) return;
-
-    this.standbyIndicator = document.createElement("DIV");
-    document.body.appendChild(this.standbyIndicator);
-    this.standbyIndicator.id = "standbyIndicator";
-  }
-
-  static hide() {
-    if (!this.standbyIndicator) return;
-
-    document.body.removeChild(this.standbyIndicator);
-    this.standbyIndicator = null;
-  }
-}*/
 export class StandbyIndicator {
-  standbyIndicator = null;
-
   static show() {
     let standbyIndicator = document.getElementById("standbyIndicator");
     if (standbyIndicator) return;
