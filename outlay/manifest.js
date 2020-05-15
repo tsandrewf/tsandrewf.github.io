31a97d1ae94610ef2dd276d165f52e0d39fffddb
tsandrewf <tsandrewf@gmail.com>
2020-05-15 18:34:11 +0300

    Localization ho

--- outlay/manifest.js
+++ outlay/manifest.js
@@ -56,6 +56,11 @@ const manifestLocales = {
     short_name: "लागत",
     description: "व्यक्तिगत वित्तीय खर्च के लिए लेखांकन",
   },
+  ho: {
+    name: "Troškovi",
+    short_name: "Troškovi",
+    description: "Računovodstvo osobnih financijskih troškova",
+  },
   hu: {
     name: "Költségek",
     short_name: "Költségek",
