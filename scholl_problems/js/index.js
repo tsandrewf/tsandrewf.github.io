a6b6ce631c193a0ff93608b1f6a404fc8928b339
tsandrewf <tsandrewf@gmail.com>
2021-05-22 17:40:59 +0300

    sevice worker

--- scholl_problems/js/index.js
+++ scholl_problems/js/index.js
@@ -3,6 +3,30 @@
 let urlToNavigate;
 let historyDepth = 0;
 
+window.onload = function () {
+  /* Only register a service worker if it's supported */
+  // https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
+  if ("serviceWorker" in navigator) {
+    const serviceWorker = "./sw.js";
+    console.log('Registering of Service Worker "' + serviceWorker + '"');
+    /*navigator.serviceWorker
+      .register(serviceWorker)
+      .then(navigator.serviceWorker.ready)
+      .then(function () {
+        console.log(
+          "navigator.serviceWorker.controller",
+          navigator.serviceWorker.controller
+        );
+        // ToDo!
+        // Почему-то в EDGE при первом запуске после регистрации worker-а
+        // navigator.serviceWorker.controller = null
+      });*/
+    navigator.serviceWorker.register(serviceWorker).then(function (reg) {
+      console.log("Registration succeeded. Scope is " + reg.scope);
+    });
+  }
+};
+
 window.navbarOnClick = function (navbarButton) {
   if ("navbar-button-active" == navbarButton.className) return;
 
@@ -34,8 +58,8 @@ window.onpopstate = function (event) {
 window.Navigate = function (url) {
   historyDepth++;
 
-  const elemContentWindowLocation = document.getElementById("content")
-    .contentWindow.location;
+  const elemContentWindowLocation =
+    document.getElementById("content").contentWindow.location;
   history.replaceState(
     {
       url:
---
+++ scholl_problems/sw.js
@@ -0,0 +1,103 @@
+"use strict";
+
+// https://www.pwabuilder.com/publish
+// https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
+
+const CACHE_NAME = "scholl_problems_v_202105221737";
+
+let cacheUrls = [
+  // HTML
+  "./index.html",
+  "./problem_with_decision.html",
+  "./class1/index.html",
+  "./class7/index.html",
+  "./class7/20210424_qualifying_round/index.html",
+  "./class7/20210424_qualifying_round/problem1.html",
+  "./class7/20210424_qualifying_round/problem2.html",
+  "./class7/20210424_qualifying_round/problem3.html",
+  "./class7/20210424_qualifying_round/problem4.html",
+  "./class7/20210424_qualifying_round/problem5.html",
+  "./class7/20210424_qualifying_round/problem6.html",
+  "./class7/20210424_qualifying_round/problem7.html",
+  "./class7/20210424_qualifying_round/problem8.html",
+  // CSS
+  "./css/index.css",
+  "./css/problem.css",
+  "./css/problem_group.css",
+  "./css/problem_with_decision.css",
+  // JS
+  "./js/getQueryVar.js",
+  "./js/index.js",
+  "./js/problem_group.js",
+  "./js/problem_list.js",
+  "./js/problem_with_decision.js",
+  // PNG
+  "./icons/scholl_problems32.png",
+  "./icons/scholl_problems48.png",
+  "./icons/scholl_problems64.png",
+  "./icons/scholl_problems72.png",
+  "./icons/scholl_problems96.png",
+  "./icons/scholl_problems128.png",
+  "./icons/scholl_problems192.png",
+  "./icons/scholl_problems256.png",
+  "./icons/scholl_problems512.png",
+  // Fonts
+  "/fonts/MaterialIcons-Regular.woff",
+];
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(cacheUrls);
+    })()
+  );
+});
+
+/*self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      Promise.all(
+        keyList.map((key) => {
+          if (key === CACHE_NAME) {
+            return;
+          }
+          caches.delete(key);
+        })
+      );
+    })()
+  );
+});*/
+// https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers#Updates
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(CACHE_NAME);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
+});
