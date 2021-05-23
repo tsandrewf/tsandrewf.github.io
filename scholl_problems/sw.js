08919024191bb125266ee00b44fcc6fa4e4db5f3
tsandrewf <tsandrewf@gmail.com>
2021-05-23 13:50:39 +0300

    Update sw.js

--- scholl_problems/sw.js
+++ scholl_problems/sw.js
@@ -3,7 +3,7 @@
 // https://www.pwabuilder.com/publish
 // https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
 
-const CACHE_NAME = "scholl_problems_v_202105231339";
+const CACHE_NAME = "scholl_problems_v_202105231350";
 
 let cacheUrls = [
   "./",
