5c5f58b7ac5e90809e866fdd13fe0e607a3ee218
tsandrewf <tsandrewf@gmail.com>
2021-06-07 13:00:31 +0300

    Update sw.js

--- scholl_problems/sw.js
+++ scholl_problems/sw.js
@@ -3,7 +3,7 @@
 // https://www.pwabuilder.com/publish
 // https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
 
-const CACHE_NAME = "scholl_problems_v_202106071021";
+const CACHE_NAME = "scholl_problems_v_202106071022";
 
 let cacheUrls = [
   "./",
@@ -115,7 +115,7 @@ self.addEventListener("message", (event) => {
   console.log("[Service Worker] message", event.data);
   if (event.data.serviceWorker === "skipWaiting") {
     skipWaiting().then(function () {
-      console.log("skipWaiting()");
+      console.log("[Service Worker] skipWaiting() OK!");
     });
   }
 });
